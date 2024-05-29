be101fbed6d175aa999aabf1603ae4aff9fbcc0e
nt2311-vn <nguyentoan231196@gmail.com>
2024-05-29 17:22:21 +0700

    Complete E46 Dig into optional value as threaten value and .? to safe cast unreachable null value

--- exercises/046_optionals2.zig
+++ exercises/046_optionals2.zig
@@ -21,18 +21,25 @@ const std = @import("std");
 
 const Elephant = struct {
     letter: u8,
-    tail: *Elephant = null, // Hmm... tail needs something...
+    tail: ?*Elephant = null, // Hmm... tail needs something...
     visited: bool = false,
 };
 
 pub fn main() void {
-    var elephantA = Elephant{ .letter = 'A' };
-    var elephantB = Elephant{ .letter = 'B' };
-    var elephantC = Elephant{ .letter = 'C' };
+    var elephantA = Elephant{
+        .letter = 'A',
+    };
+    var elephantB = Elephant{
+        .letter = 'B',
+    };
+    var elephantC = Elephant{
+        .letter = 'C',
+    };
 
     // Link the elephants so that each tail "points" to the next.
     elephantA.tail = &elephantB;
     elephantB.tail = &elephantC;
+    elephantC.tail = &elephantA;
 
     visitElephants(&elephantA);
 
@@ -51,6 +58,6 @@ fn visitElephants(first_elephant: *Elephant) void {
         // We should stop once we encounter a tail that
         // does NOT point to another element. What can
         // we put here to make that happen?
-        e = e.tail ???
+        e = e.tail.?;
     }
 }
