3bcbbcbb1f0a0e6c3b00de9471ef15149b6190e1
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-06 16:46:50 +0700

    Complete E65 builtin

--- exercises/065_builtins2.zig
+++ exercises/065_builtins2.zig
@@ -67,7 +67,6 @@ pub fn main() void {
 
     // Oops! We cannot leave the 'me' and 'myself' fields
     // undefined. Please set them here:
-
     narcissus.me = &narcissus;
     narcissus.myself = &narcissus;
 
@@ -81,7 +80,7 @@ pub fn main() void {
     //
     // The fix for this is very subtle, but it makes a big
     // difference!
-    const Type2 = narcissus.fetchTheMostBeautifulType();
+    const Type2 = Narcissus.fetchTheMostBeautifulType();
 
     // Now we print a pithy statement about Narcissus.
     print("A {s} loves all {s}es. ", .{
@@ -120,15 +119,15 @@ pub fn main() void {
     // Please complete these 'if' statements so that the field
     // name will not be printed if the field is of type 'void'
     // (which is a zero-bit type that takes up no space at all!):
-    if (fields[0].field_type != void) {
+    if (fields[0].type != void) {
         print(" {s}", .{@typeInfo(Narcissus).Struct.fields[0].name});
     }
 
-    if (fields[1].field_type != void) {
+    if (fields[1].type != void) {
         print(" {s}", .{@typeInfo(Narcissus).Struct.fields[1].name});
     }
 
-    if (fields[2].field_type != void) {
+    if (fields[2].type != void) {
         print(" {s}", .{@typeInfo(Narcissus).Struct.fields[2].name});
     }
 
